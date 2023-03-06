86e2f12204d65c698ef9fb753b2fd5fc893b04f1
harleymacedo <harley.ip@gmail.com>
2023-03-06 10:25:51 -0300

    Exemplo API Mock melhorado

---
+++ NodeExpress/APIMock/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const professorRouter = require('./routes/professorRouter');
+
+app.use(express.json());
+app.use(professorRouter);
+
+app.listen(3000);
---
+++ NodeExpress/APIMock/routes/professorRouter.js
@@ -0,0 +1,66 @@
+const professorRouter = require('express').Router();
+var professores = [
+    {nome: 'Marcos', area: 'Banco de dados'},
+    {nome: 'Ana', area: 'UX Design'},
+    {nome: 'Hugo', area: 'Programação'},
+    {nome: 'Diego', area: 'Inteligência artificial'},
+    {nome: 'Maria', area: 'Programação'},
+    {nome: 'Hugo', area: 'Engenharia de software'},
+    {nome: 'Danilo', area: 'Programação web e mobile'},
+    {nome: 'Yolanda', area: 'Engenharia de software'},
+];
+
+professorRouter.get('/professor/todos', (req, res) => {
+    try {
+        res.json(professores);
+    } catch (error) {
+        res.json({erro: true, mensagem: 'Não foi possivel recuperar os dados.'});
+    }
+});
+
+professorRouter.get('/professor/:nome', (req, res) => {
+    try {
+        const professorEncontrado = professores.find( (item) => {
+            return item.nome === req.params.nome;
+        });
+        res.json(professorEncontrado);
+    } catch (error) {
+        res.json({erro: true, mensagem: 'Não foi possivel recuperar os dados.'});
+    }
+});
+
+professorRouter.post('/professor', (req, res) => {
+    try {
+        const novoProfessor = {nome: req.body.nome, area: req.body.area};
+        professores.push(novoProfessor);
+        res.json({mensagem: 'Professor cadastrado com sucesso'});
+    } catch (error) {
+        res.json({mensagem: 'Erro durante o cadastro', erro: true});
+    }
+});
+
+professorRouter.put('/professor', (req, res) => {
+    try {
+        professores.forEach( (objetoAtual) => {
+            if (objetoAtual.nome === req.body.nome) {
+                objetoAtual.area = req.body.area;
+            }
+        });
+        res.json({mensagem: 'Professor atualizado com sucesso'});
+    } catch (error) {
+        res.json({mensagem: 'Erro durante a atualização', erro: true});
+    }
+});
+
+professorRouter.delete('/professor', (req, res) => {
+    try {
+        professores = professores.filter( (objetoAtual) => {
+            return (objetoAtual.nome !== req.body.nome);
+        });
+        res.json({mensagem: 'Professor excluído com sucesso'});
+    } catch (error) {
+        res.json({mensagem: 'Erro durante a exclusão', erro: true});
+    }
+});
+
+module.exports = professorRouter;
