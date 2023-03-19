1a91a268f6c27f8e30eff233660c2beec37ba971
harleymacedo <harley.ip@gmail.com>
2023-03-18 23:23:27 -0300

    Function example added.

---
+++ Typescript/TiposAvancados/functions.ts
@@ -0,0 +1,13 @@
+function getTime (): number {
+    return new Date().getTime();
+}
+
+//Optional parameters
+function add(a: number, b: number, c?: number) {
+    return a + b + (c || 0);
+}
+
+//Default parameters
+function pow(value: number, expoent: number = 10) {
+    return value ** expoent;
+}
