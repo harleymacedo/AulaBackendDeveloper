f847cc1d9bc6fa28c07c136a793489fdd11e4c3f
harleymacedo <harley.ip@gmail.com>
2023-03-26 21:58:40 -0300

    Rotas completas

--- Aula3DeployVersionamento/APIMockDisciplinas/routes/disciplinaRouter.js
+++ Aula3DeployVersionamento/APIMockDisciplinas/routes/disciplinaRouter.js
@@ -4,3 +4,10 @@ const disciplinas = require('../model/disciplinaModel');
 disciplinaRouter.get('/disciplina/todas', (req, res) => {
     res.json(disciplinas);
 });
+
+disciplinaRouter.get('/disciplina/codigo/:codigo', (req, res) => {
+    const disciplinaBuscada = disciplinas.find( (objetoAtual) => {
+        return (objetoAtual.codigo === req.params.codigo);
+    });
+    res.json(disciplinaBuscada);
+});
