0c3cf596c19f2b68c3b874dfb6fa0abb3147c258
harleymacedo <harley.ip@gmail.com>
2023-04-08 23:11:21 -0300

    Teste de post para professor

--- Aula5TesteDocumentacao/TesteComJest/test/professor.test.js
+++ Aula5TesteDocumentacao/TesteComJest/test/professor.test.js
@@ -7,11 +7,19 @@ test('GET /professor/todos', async () => {
 });
 
 test('GET /professor/nome/:nome', async () => {
+    const result = await axios.get('http://localhost:3001/professor/nome/Ana');
+    expect(result.data.professor).toBeTruthy();
+    expect(result.data.professor.nome).toBeTruthy();
+});
+
+test('POST /professor', async () => {
     await axios.post('http://localhost:3001/professor', {
         "nome": "Matheus",
         "area": "Gerência de projetos"
     });
-    const result = await axios.get('http://localhost:3001/professor/nome/Matheus');
+    const result = await axios.get('http://localhost:3001/professor/nome/Ana');
     expect(result.data.professor).toBeTruthy();
     expect(result.data.professor.nome).toBeTruthy();
 });
+
+// test('PUT /professor', asy)
