7ab6515fa82860cb7b07f084f6d0c89c16d3fe87
harleymacedo <harley.ip@gmail.com>
2023-04-18 09:53:26 -0300

    Exemplos de string adicionados

---
+++ Aula1JavascriptBackend/FuncoesParaString/FuncoesParaString.js
@@ -0,0 +1,8 @@
+//Dividir split
+var texto1 = 'O curso de Sistemas de Informação do IFCE Campus Crato';
+var palavras1 = texto1.split(' ');
+console.log(palavras1);
+
+//Quantidade de caracteres
+var qtdCarac = texto1.length;
+console.log(`${qtdCarac} caracteres`);
