7cc30726646c4f443560b9adb70cea580ff41376
harleymacedo <harley.ip@gmail.com>
2023-04-22 23:47:59 -0300

    Nova estrutura criada

--- Aula6TypeScript/TiposExpress/src/index.ts
+++ Aula6TypeScript/TiposExpress/src/index.ts
@@ -5,8 +5,10 @@ const app = express();
 const route = Router();
 
 app.use(express.json());
+app.use(route);
 
 route.get('/', (req: Request, res: Response) => {
     res.json({message: 'Hello World with Typescript'});
 });
 
+app.listen(3000, () => 'Server running on port 3000');
