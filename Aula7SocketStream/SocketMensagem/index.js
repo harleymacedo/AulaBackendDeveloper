0f9973b09eec65bf5ed2f4fa7d9d29dd8f795580
harleymacedo <harley.ip@gmail.com>
2023-05-08 15:19:07 -0300

    Exemplo de socket de texto adicionado

---
+++ Aula7SocketStream/SocketMensagem/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const http = require('http')
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/temperatura', (req, res) => {
+  res.sendFile(__dirname + '/indexTemperatura.html');
+});
+
+io.on('connection', (socket) => {
+  console.log("novo usuário conectado");
+  socket.on('mensageiro', (msg) => {
+    io.emit('mensageiro', msg);
+  });
+
+  socket.on('sincronizarConteudo', (conteudo) => {
+    io.emit('sincronizarConteudo', conteudo);
+  });
+
+  socket.on('mudouTemperatura', (novaTemperatura) => {
+    console.log('servidor notificado: mudouTemperatura');
+    io.emit('mudouTemperatura', novaTemperatura);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}/`);
+});
