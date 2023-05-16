e1b1c3d16575271efb31d3ebb1c6ec0d2b9c714f
harleymacedo <harley.ip@gmail.com>
2023-05-16 09:54:37 -0300

    Tipo tupla adicionado

--- Aula6TypeScript/TipoArray/index.js
+++ Aula6TypeScript/TipoArray/index.js
@@ -2,3 +2,6 @@
 var names = [];
 names.push("Dylan");
 //names.push(3);
+var numbers = [1, 2, 3]; //inferred to type number[]
+numbers.push(4); // no error
+//numbers.push("2"); // Error:
--- Aula6TypeScript/TipoArray/index.ts
+++ Aula6TypeScript/TipoArray/index.ts
@@ -1,5 +1,13 @@
-
 //Tipo array
 const names: string[] = [];
 names.push("Dylan");
 //names.push(3);
+
+//Tipagem por inferência
+const numbers = [1, 2, 3]; //tipo definido como number[]
+numbers.push(4); //sem erro
+//numbers.push("2"); //com erro
+
+//Tupla definida
+let ourTuple: [number, boolean, string];
+ourTuple = [5, false, 'Exemplo de tupla']; //Inicialização correta
