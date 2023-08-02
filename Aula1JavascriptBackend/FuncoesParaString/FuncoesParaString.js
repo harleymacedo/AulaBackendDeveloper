9bd6052045dfa90f89b6f8785fbc370e33c3abb6
harleymacedo <harley.ip@gmail.com>
2023-08-02 11:42:21 -0300

    Declaração de função

--- Aula1JavascriptBackend/FuncoesParaString/FuncoesParaString.js
+++ Aula1JavascriptBackend/FuncoesParaString/FuncoesParaString.js
@@ -1,8 +1,8 @@
 //Dividir split
-var texto1 = 'O curso de Sistemas de Informação do IFCE Campus Crato';
-var palavras1 = texto1.split(' ');
-console.log(palavras1);
+var texto1 = 'O curso de Sistemas de Informação do IFCE Campus Crato'
+var palavras1 = texto1.split(' ')
+console.log(palavras1)
 
 //Quantidade de caracteres
-var qtdCarac = texto1.length;
-console.log(`${qtdCarac} caracteres`);
+var qtdCarac = texto1.length
+console.log(`${qtdCarac} caracteres`)
