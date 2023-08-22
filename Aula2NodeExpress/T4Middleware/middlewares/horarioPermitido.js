c36b9e0aa1cfcdabab375289d962988c17562c25
harleymacedo <harley.ip@gmail.com>
2023-08-22 08:22:38 -0300

    Callback com passagem de dados

--- Aula2NodeExpress/T4Middleware/middlewares/horarioPermitido.js
+++ Aula2NodeExpress/T4Middleware/middlewares/horarioPermitido.js
@@ -1,10 +1,10 @@
 const confereHorario = (req, res, next) => {
-    let date = new Date();
-    let hora = date.getHours();
+    let date = new Date()
+    let hora = date.getHours()
     if (hora >= 8) {
-        return res.json({mensagem: 'Nesse horário não é permitido fazer essa requisição'});
+        return res.json({mensagem: 'Nesse horário não é permitido fazer essa requisição'})
     }
-    next();
+    next()
 }
 
-module.exports = confereHorario;
+module.exports = confereHorario
