9f8a42ee3bb7015d2cac0b9c52753c275c10b603
harleymacedo <harley.ip@gmail.com>
2023-08-25 16:02:52 -0300

    Upload de arquivo, mas com pendência

--- Aula2NodeExpress/T6BibliotecasImportantes/indexUpload.js
+++ Aula2NodeExpress/T6BibliotecasImportantes/indexUpload.js
@@ -1,19 +1,21 @@
 //Importando e instanciando o ExpressJS
 const express = require('express')
 const app = express()
+const multer = require('multer')
 
 //Uso de middleware
 app.use(express.static('public'))
 
+//Inicializando o multer
+const upload = multer({dest: 'uploads/'})
+
 //Definindo rotas com suas devidas respostas
-app.get('/recomendacao', (req, res) => {
-    res.json({recomendacao: 'Lendas da paixão'})
-})
-app.get('/lancamentos', (req, res) => {
-    res.json({recomendacao: ['7 anos no Tibet', 'Tempo de glória']})
-})
 app.get('/cadastro', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
+})
+
+app.post('/recebeDados', upload.single('file'), (req, res) => {
+    res.json({mensagem: 'Arquivo armazenado'})
 })
 
 //Ouvinte das requisições
