2682db2eab87749b99e022a2582212a0551801c6
harleymacedo <harley.ip@gmail.com>
2023-08-26 11:59:32 -0300

    Upload melhorado

--- Aula2NodeExpress/T6BibliotecasImportantes/indexUpload.js
+++ Aula2NodeExpress/T6BibliotecasImportantes/indexUpload.js
@@ -6,14 +6,28 @@ const multer = require('multer')
 //Uso de middleware
 app.use(express.static('public'))
 
-//Inicializando o multer
-const upload = multer({dest: 'uploads/'})
+// Configuração de armazenamento
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        // Extração da extensão do arquivo original:
+        const extensaoArquivo = file.originalname.split('.')[1]
+        // Cria um código randômico que será o nome do arquivo
+        const novoNomeArquivo = require('crypto')
+            .randomBytes(64)
+            .toString('hex')
+        // Indica o novo nome do arquivo:
+        cb(null, `${novoNomeArquivo}.${extensaoArquivo}`)
+    }
+});
+const upload = multer({ storage })
 
 //Definindo rotas com suas devidas respostas
 app.get('/cadastro', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
-
 app.post('/recebeDados', upload.single('file'), (req, res) => {
     res.json({mensagem: 'Arquivo armazenado'})
 })
