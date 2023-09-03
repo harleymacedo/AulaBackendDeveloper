97bdc7645dbd97871937a217bc07e2f051f63431
harleymacedo <harley.ip@gmail.com>
2023-09-02 23:50:39 -0300

    Rotas iniciadas

--- Aula6Seguranca/index.js
+++ Aula6Seguranca/index.js
@@ -1,4 +1,21 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
+
+app.get('/login', (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+app.delete('/usuario', (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
 
 app.listen(3000)
