d7e21f09e2564b2fcaaaed8fce14c6d1c60e7f6d
harleymacedo <harley.ip@gmail.com>
2023-09-15 18:46:28 -0300

    Segurança melhorada

--- Aula6Seguranca/index.js
+++ Aula6Seguranca/index.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
+const dotenv = require('dotenv').config()
+
+
+const verificarJWT = (req, res, next) => {
+    const token = req.body.token
+    if (!token) {
+        res.json({logado: false, mensagem: 'Token não foi enviado.'})
+    }
+    jwt.verify(token, process.env.APP_KEY, (err, decoded) => {
+        if (err) {
+            res.json({locado: false, mensagem: 'Falha na autenticação'})
+        }
+    })
+    next()
+}
 
 app.get('/login', (req, res) => {
     try {
@@ -12,9 +28,14 @@ app.get('/login', (req, res) => {
 
 app.post('/validaLogin', (req, res) => {
     try {
-        
+        const {usuario, senha} = req.body;
+        if (usuario === process.env.USUARIO && senha === process.env.SENHA) {
+            let novoToken = jwt.sign({usuario}, process.env.APP_KEY, {expiresIn: 9000})
+            res.json({logado: true, token: novoToken})
+        }
+        res.json({logado: false, mensagem: 'Usuário ou senha errados.'});        
     } catch (error) {
-        
+        res.json({logado: false, mensagem: 'Erro durante o login.'});        
     }
 })
 
