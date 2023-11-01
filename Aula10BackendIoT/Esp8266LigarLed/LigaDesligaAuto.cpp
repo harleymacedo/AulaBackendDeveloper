763e578aa8b8040af373b14740730de4afa4782a
harleymacedo <harley.ip@gmail.com>
2023-11-01 09:53:28 -0300

    Exemplo IoT, com liga e desliga auto

---
+++ Aula10BackendIoT/Esp8266LigarLed/LigaDesligaAuto.cpp
@@ -0,0 +1,13 @@
+void setup() {
+  // Definir portas e pinos
+  pinMode(D1, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(D1, HIGH);
+  delay(1000);
+
+  digitalWrite(D1, LOW);
+  delay(1000);
+}
