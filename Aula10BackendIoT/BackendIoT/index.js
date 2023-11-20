0a913b75adb6f8002031204be61e2811fcffb20b
harleymacedo <harley.ip@gmail.com>
2023-11-19 21:29:27 -0300

    Aplicação para integração com IoT iniciada

---
+++ Aula10BackendIoT/BackendIoT/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+
+var temperaturaAtual = 30.0
+
+app.use(express.json())
+
+app.get('/temperatura', (req, res) => {
+    res.json({valorTemperatura: temperaturaAtual})
+})
+
+app.post('/temperatura', (req, res) => {
+    temperaturaAtual = req.params.novaTemperatura
+    res.json({mensagem: 'Temperatura atualizada'})
+})
+
+app.listen(process.env.PORT || 3000)
