1fd00d5822371a6b4e077874912b6e266cd5786b
harleymacedo <harley.ip@gmail.com>
2023-12-14 23:54:28 -0300

    Exemplo para deploy e versionamento melhorado

--- Aula3VersionamentoDeploy/Ex2APIMockTimes/index.js
+++ Aula3VersionamentoDeploy/Ex2APIMockTimes/index.js
@@ -1,8 +1,37 @@
+//Importações gerais
 const express = require('express')
 const app = express()
+var times = require('./times')
 
+//Rota para teste simples
 app.get('/', (req, res) => {
     res.json({'mensagem': 'Rota / acionada'})
 })
 
+//Rota get para obter todos os times
+app.get('/times/todos', (req, res) => {
+    res.json({"times": times})
+})
+
+//Rota get para obter um time pelo nome
+app.get('/times/:nome', (req, res) => {
+    res.json({"time": null})
+})
+
+//Rota post para inserir um novo time
+app.post('/times/inserir', (req, res) => {
+    res.json({"mensagem": "Novo time inserido"})
+})
+
+//Rota update para atualizar um time, pelo nome
+app.update('/times/atualizar', (req, res) => {
+    res.json({"mensagem": "Time atualizado"})
+})
+
+//Rota delete para excluir um time, pelo nome
+app.delete('/times/excluir', (req, res) => {
+    res.json({"mensagem": "Time exluido"})
+})
+
+//Ouvinte de requisições
 app.listen(3000)
---
+++ Aula3VersionamentoDeploy/Ex2APIMockTimes/times.js
@@ -0,0 +1,28 @@
+var times = [
+    {
+        "nome": "Palmeiras",
+        "pontos": 49
+    },
+    {
+        "nome": "Flamengo",
+        "pontos": 47
+    },
+    {
+        "nome": "Grêmio",
+        "pontos": 45
+    },
+    {
+        "nome": "Bragantino",
+        "pontos": 42
+    },
+    {
+        "nome": "Botafogo",
+        "pontos": 49
+    },
+    {
+        "nome": "Fortaleza",
+        "pontos": 49
+    },
+]
+
+module.exports = times
