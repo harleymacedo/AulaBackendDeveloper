da1b85d0ef309f4926952eeb6dc3b908ecb41ed2
harleymacedo <harley.ip@gmail.com>
2023-12-26 15:23:49 -0300

    Obtendo e inserindo times corretamente

--- Aula3VersionamentoDeploy/Ex2APIMockTimes/index.js
+++ Aula3VersionamentoDeploy/Ex2APIMockTimes/index.js
@@ -3,35 +3,42 @@ const express = require('express')
 const app = express()
 var times = require('./times')
 
+//Middleware para utilizar body da requisição
+app.use(express.json())
+
 //Rota para teste simples
 app.get('/', (req, res) => {
     res.json({'mensagem': 'Rota / acionada'})
 })
 
 //Rota get para obter todos os times
-app.get('/times/todos', (req, res) => {
+app.get('/time/todos', (req, res) => {
     res.json({"times": times})
 })
 
 //Rota get para obter um time pelo nome
-app.get('/times/:nome', (req, res) => {
+app.get('/time/nome/:nome', (req, res) => {
     const timeBuscado = times.find( (elemento) => elemento.nome == req.params.nome )
     res.json({"time": timeBuscado})
 })
 
 //Rota post para inserir um novo time
-app.post('/times/inserir', (req, res) => {
-    times.push({nome: req.params.nome, pontos: req.params.pontos})
+app.post('/time/inserir', (req, res) => {
+    times.push({nome: req.body.nome, pontos: req.body.pontos})
     res.json({"mensagem": "Novo time inserido"})
 })
 
 //Rota put para atualizar um time, pelo nome
-app.put('/times/atualizar', (req, res) => {
+app.put('/time/atualizar', (req, res) => {
+    timeProcurado = times.find( (elemento) => elemento.nome == req.params.nome )
+    if (timeProcurado != null) {
+        timeProcurado.pontos = req.params.pontos
+    }
     res.json({"mensagem": "Time atualizado"})
 })
 
 //Rota delete para excluir um time, pelo nome
-app.delete('/times/excluir', (req, res) => {
+app.delete('/time/excluir', (req, res) => {
     res.json({"mensagem": "Time exluido"})
 })
 
