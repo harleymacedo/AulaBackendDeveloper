d3e7b162063398b5e88f1b91f8d2c57a23324495
harleymacedo <harley.ip@gmail.com>
2023-12-28 14:46:26 -0300

    Código comentado em index

--- Aula3VersionamentoDeploy/Ex1APIMockDisciplinas/index.js
+++ Aula3VersionamentoDeploy/Ex1APIMockDisciplinas/index.js
@@ -1,10 +1,16 @@
+//Imports gerais
 const express = require('express')
 const app = express()
+//Biblioteca para configurar e reconhecer as variáveis no arquivo .env
 const dotenv = require('dotenv').config()
 const disciplinaRouter = require('./routes/disciplinaRouter')
+//Biblioteca para configurar o acesso à aplicações por outros servidores
 const cors = require('cors')
 
+//Middleware para reconhecer response json
 app.use(express.json())
+//Middleware para rotear as requisições
 app.use(disciplinaRouter)
 
+//Atendendte de requisições, a porta pode ser definida automaticamente pelo servidor
 app.listen(process.env.PORT || 3000)
