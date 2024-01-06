af9e3b559f2a0f198bb9315a26c58f75430cab50
harleymacedo <harley.ip@gmail.com>
2024-01-05 21:56:02 -0300

    Cors adicionado na aplicação de disciplinas

--- Aula3VersionamentoDeploy/Ex1APIMockDisciplinas/index.js
+++ Aula3VersionamentoDeploy/Ex1APIMockDisciplinas/index.js
@@ -1,11 +1,14 @@
 //Imports gerais
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 //Biblioteca para configurar e reconhecer as variáveis no arquivo .env
 const dotenv = require('dotenv').config()
 const disciplinaRouter = require('./routes/disciplinaRouter')
 
+//Habilitando o cors para autorizar acesso desta API por frontend de outro servidor
+app.use(cors)
 //Middleware para reconhecer response json
 app.use(express.json())
 //Middleware para rotear as requisições
