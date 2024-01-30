809cb668721c66114a5343f8c2d3b46ac17239b8
harleymacedo <harley.ip@gmail.com>
2024-01-30 15:40:03 -0300

    Exemplos de array com funções arrow

--- Aula1JavascriptBackend/Ex9FuncoesParaArray/FuncoesParaArray2.js
+++ Aula1JavascriptBackend/Ex9FuncoesParaArray/FuncoesParaArray2.js
@@ -1,10 +1,15 @@
 //Uso da função map
 var studios = ['Warner Bros', '21 Century', 'Sony']
-studios.map( function (s) { console.log(s.length) } )
+var quantidades = studios.map( (valor) => { 
+    console.log(valor.length) 
+    return valor.length
+})
+console.log(quantidades)
 
 //Uso da função reduce
 var numeros = [8, 9, 4, 5, 9, 9]
-var soma = numeros.reduce(function (total, numero) { 
+var soma = numeros.reduce( (total, numero) => { 
+    console.log(total, numero)
     return total + numero 
-} )
+})
 console.log(soma)
