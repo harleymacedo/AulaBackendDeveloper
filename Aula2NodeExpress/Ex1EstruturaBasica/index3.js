922e15f3846175c3888cfbc183a2835e67d6d6ba
harleymacedo <harley.ip@gmail.com>
2024-03-21 22:05:31 -0300

    Exemplo de integração backend e frontend

---
+++ Aula2NodeExpress/Ex1EstruturaBasica/index3.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const times = [
+  {id: 1, nome: 'Flamengo', cidade: 'Crato'},
+  {id: 2, nome: 'Palmeiras', cidade: 'Juazeiro'},
+  {id: 3, nome: 'Botafogo', cidade: 'Crato'},
+  {id: 4, nome: 'Bahia', cidade: 'Barbalha'},
+  {id: 5, nome: 'Santos', cidade: 'Crato'},
+]
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/home.html')
+})
+
+app.get('/times', function (req, res) {
+  res.json(times)
+})
+
+app.get('/times/:cidade', function (req, res) {
+  let cidade = req.params.cidade
+  let timesCidade = times.filter(function (time){
+    return time.cidade === cidade
+  } )
+  res.json(timesCidade)
+})
+
+app.listen(3000)
