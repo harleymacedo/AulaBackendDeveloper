f875d17ad90857f1e7f7c8ce71a1414121a7854a
harleymacedo <harley.ip@gmail.com>
2024-08-20 08:11:33 -0300

    Alterando exemplos de Typescript

---
+++ Aula7TypeScript/Ex1Funcoes/index.js
@@ -0,0 +1,8 @@
+function somar(num1, num2) {
+    return num1 + '' + num2;
+}
+function somar2(num1, num2) {
+    return num1 + num2;
+}
+console.log(somar(1, 2));
+console.log(somar2(2, 3));
--- Aula7TypeScript/TiposBasicos/index.js
+++ Aula7TypeScript/TiposBasicos/index.js
@@ -7,12 +7,3 @@ var car2 = {
     type: 'Toyota'
 };
 car2.mileage = 2000;
-//Data type Enum
-var CardinalDirections;
-(function (CardinalDirections) {
-    CardinalDirections[CardinalDirections["North"] = 1] = "North";
-    CardinalDirections[CardinalDirections["East"] = 2] = "East";
-    CardinalDirections[CardinalDirections["South"] = 3] = "South";
-    CardinalDirections[CardinalDirections["West"] = 4] = "West";
-})(CardinalDirections || (CardinalDirections = {}));
-console.log(CardinalDirections.West);
