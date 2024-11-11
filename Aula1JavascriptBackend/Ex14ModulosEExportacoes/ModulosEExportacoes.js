be8f8c78994221f9a6abf43e64683b3145dca0fe
harleymacedo <harley.ip@gmail.com>
2024-11-11 16:05:52 -0300

    Modulos e exportações modificado

--- Aula1JavascriptBackend/Ex14ModulosEExportacoes/ModulosEExportacoes.js
+++ Aula1JavascriptBackend/Ex14ModulosEExportacoes/ModulosEExportacoes.js
@@ -6,7 +6,3 @@ console.log(robo.nome)
 //Obtendo mais de uma variável
 const {nomes, cidades} = require('./Auxiliar2')
 console.log(nomes, cidades)
-
-// import salas from './Auxilizar3.js'
-// console.log(salas)
-//"type": "module", (colocar no package.json)
---
+++ Aula1JavascriptBackend/Ex14ModulosEExportacoes/ModulosEExportacoes2.js
@@ -0,0 +1,5 @@
+//Padrão ES import/export
+//"type": "module", (colocar no package.json)
+
+import salas from './Auxilizar3.js'
+console.log(salas)
