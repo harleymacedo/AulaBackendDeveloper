416e62b5b8e375b7fd565327f367bc1ef02cdbbc
harleymacedo <harley.ip@gmail.com>
2024-12-17 16:32:17 -0300

    Cors com lugar alterado

--- Aula3VersionamentoDeploy/Ex2APIMockTimes/index.js
+++ Aula3VersionamentoDeploy/Ex2APIMockTimes/index.js
@@ -6,12 +6,7 @@ const cors = require('cors')
 //Biblioteca para configurar e reconhecer as variáveis no arquivo .env
 const dotenv = require('dotenv').config()
 //Permissão de acesso
-app.use(cors(
-    {
-        "origin": "*",
-        "methods": "GET,PUT,POST,DELETE",
-    }
-))
+
 //Lista de times mockado
 var times = require('./model/times')
 
@@ -71,6 +66,13 @@ app.delete('/time/excluir', (req, res) => {
         res.json({"mensagem": "Time não localizado para exclusão"})
     }
 })
+
+app.use(cors(
+    {
+        "origin": "*",
+        "methods": "GET,PUT,POST,DELETE",
+    }
+))
 
 //Atendendte de requisições, a porta pode ser definida automaticamente pelo servidor
 app.listen(process.env.PORT || 3000)
