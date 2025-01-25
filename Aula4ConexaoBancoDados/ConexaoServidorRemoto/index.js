c268d3f69e5a05ca809f85d4ed630b8b88f7fb8b
harleymacedo <harley.ip@gmail.com>
2025-01-24 23:09:08 -0300

    Conexão melhorada

--- Aula4ConexaoBancoDados/ConexaoServidorRemoto/index.js
+++ Aula4ConexaoBancoDados/ConexaoServidorRemoto/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
-const disciplinaRouter = require('./routers/disciplinaRouter');
-const dotenv = require('dotenv').config();
+const express = require('express')
+const app = express()
+const disciplinaRouter = require('./routers/disciplinaRouter')
+const dotenv = require('dotenv').config()
 
-app.use(express.json());
-app.use(disciplinaRouter);
+app.use(express.json())
+app.use(disciplinaRouter)
 
-app.listen(3000);
+app.listen(3000)
--- Aula4ConexaoBancoDados/Ex1GruposDeEstudo/index.js
+++ Aula4ConexaoBancoDados/Ex1GruposDeEstudo/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 
+app.use(express.json())
+
 app.get('/grupos', function (req, res) {
     res.json({gruposEncontrados: []})
 })
