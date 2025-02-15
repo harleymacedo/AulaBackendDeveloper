3f70fd536459b60475bd3d091d4038efd9588e5b
harleymacedo <harley.ip@gmail.com>
2025-02-14 23:47:22 -0300

    async await adicionado funções

--- Aula4ConexaoBancoDados/Ex1GruposDeEstudo/routers/alunoRouter.js
+++ Aula4ConexaoBancoDados/Ex1GruposDeEstudo/routers/alunoRouter.js
@@ -2,51 +2,51 @@ const alunoRouter = express.Router()
 const mongoose = require('mongoose')
 const aluno = require('../models/aluno')
 
-alunoRouter.get('/aluno', function (req, res) {
+alunoRouter.get('/aluno', async function (req, res) {
     try {
-        mongoose.connect(process.env.DB_CONNECTION_STRING)
-        const alunosBuscados = aluno.find()
+        await mongoose.connect(process.env.DB_CONNECTION_STRING)
+        const alunosBuscados = await aluno.find()
         res.json({'alunos': alunosBuscados})
     } catch (error) {
         res.json({'erro': true, 'mensagem': 'Erro durante a consulta'})
     }
 })
 
-alunoRouter.get('/aluno/:id', function (req, res) {
+alunoRouter.get('/aluno/:id', async function (req, res) {
     try {
-        mongoose.connect(process.env.DB_CONNECTION_STRING)
+        await mongoose.connect(process.env.DB_CONNECTION_STRING)
         const id = req.params.id
-        const alunoBuscado = aluno.findOne({'id': id})
+        const alunoBuscado = await aluno.findOne({'id': id})
         res.json({'aluno': alunoBuscado})    
     } catch (error) {
         res.json({'erro': true, 'mensagem': 'Erro durante a consulta'})
     }
 })
 
-alunoRouter.post('/aluno', function (req, res) {
+alunoRouter.post('/aluno', async function (req, res) {
     try {
         dados = req.body
-        aluno.create(dados)
+        await aluno.create(dados)
         res.json({mensagem: 'Aluno gravado com sucesso!'})
     } catch (error) {
         res.json({'erro': true, 'mensagem': 'Erro durante a consulta'})
     }
 })
 
-alunoRouter.put('/aluno', function(req, res) {
+alunoRouter.put('/aluno', async function(req, res) {
     try {
         const alunoPassado = req.body
-        aluno.findOneAndUpdate({'id': alunoPassado.id}, alunoPassado)        
+        await aluno.findOneAndUpdate({'id': alunoPassado.id}, alunoPassado)        
         res.json({mensagem: 'Aluno atualizado com sucesso!'})
     } catch (error) {
         res.json({'erro': true, 'mensagem': 'Erro durante a consulta'})
     }
 })
 
-alunoRouter.delete('/aluno', function(req, res) {
+alunoRouter.delete('/aluno', async function(req, res) {
     try {
         const dados = req.body
-        aluno.deleteOne(dados)
+        await aluno.deleteOne(dados)
         res.json({mensagem: 'Aluno excluido com sucesso'})
     } catch (error) {
         res.json({'erro': true, 'mensagem': 'Erro durante a consulta'})
--- Aula4ConexaoBancoDados/Ex1GruposDeEstudo/routers/grupoRouter.js
+++ Aula4ConexaoBancoDados/Ex1GruposDeEstudo/routers/grupoRouter.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const grupoRouter = express.Router();
+const grupoRouter = express.Router()
+const mongoose = require('mongoose')
+const grupo = require('../models/grupo')
 
-grupoRouter.get('/grupo', function (req, res) {
-    res.json({grupos: []})
+grupoRouter.get('/grupo', async function (req, res) {
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION_STRING)
+        const gruposProcurados = await grupo.find()
+        res.json({'grupos': gruposProcurados})
+    } catch (error) {        
+        res.json({'erro': true, 'mensagem': 'Erro durante a consulta'})
+    }
 })
 
-grupoRouter.get('/grupo/:id', function (req, res) {
-    res.json({grupo: []})
+grupoRouter.get('/grupo/:id', async function (req, res) {
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION_STRING)
+        const id = req.params.id
+        const grupoProcurado = await grupo.findOne({'id': id})
+        res.json({'grupo': grupoProcurado})
+    } catch (error) {
+        res.json({'erro': true, 'mensagem': 'Erro durante a consulta'})
+    }
 })
 
 grupoRouter.post('/grupo', function (req, res) {
