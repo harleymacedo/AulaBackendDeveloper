9bc52236d7aa62d90897f8d955e7bdc66165bb95
harleymacedo <harley.ip@gmail.com>
2025-04-16 16:15:24 -0300

    Middleware adicionado em api de grupos de estudo

--- Aula4ConexaoBancoDados/Ex1GruposDeEstudo/index.js
+++ Aula4ConexaoBancoDados/Ex1GruposDeEstudo/index.js
@@ -14,6 +14,7 @@ app.use(cors(
 ))
 app.use(express.static('public'))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(alunoRouter)
 app.use(grupoRouter)
 app.use(autenticacao)
