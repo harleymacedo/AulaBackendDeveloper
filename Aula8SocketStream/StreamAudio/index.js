6916d4a3f7ac6d953af29052aff3d927fdedb0d6
harleymacedo <harley.ip@gmail.com>
2025-05-27 21:23:00 -0300

    Padronização de código e arquivo de mídia adicionado

--- Aula8SocketStream/StreamAudio/index.js
+++ Aula8SocketStream/StreamAudio/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const express = require('express')
+const app = express()
+const fs = require('fs')
 
 app.get('/', (req, res) => {
-    res.json({mensagem: 'Funcionando corretamente'});
-});
+    res.json({mensagem: 'Funcionando corretamente'})
+})
 
 app.get('/homeMusica', (req, res) => {
-    res.sendFile(__dirname + '/musicas.html');
-});
+    res.sendFile(__dirname + '/musicas.html')
+})
 
 app.get('/musica', (req, res) => {
-    const range = req.headers.range;
-    const musicaPath = './musica1.mp3';
-    const musicaSize = fs.statSync(musicaPath).size;
+    const range = req.headers.range
+    const musicaPath = './musica1.mp3'
+    const musicaSize = fs.statSync(musicaPath).size
 
-    const chunkSize = 1 * 1e+6;
-    const start = Number(range.replace(/\D/g, ''));
-    const end = Math.min(start + chunkSize, musicaSize - 1);
+    const chunkSize = 1 * 1e+6
+    const start = Number(range.replace(/\D/g, ''))
+    const end = Math.min(start + chunkSize, musicaSize - 1)
 
-    const contentLength = end - start + 1;
+    const contentLength = end - start + 1
 
     const headers = {
         "Content-Range": `bytes ${start}-${end}/${musicaSize}`,
@@ -27,12 +27,12 @@ app.get('/musica', (req, res) => {
         "Content-Length": contentLength,
         "Content-Type": "audio/mp3",
     }
-    res.writeHead(206, headers);
+    res.writeHead(206, headers)
 
-    const stream = fs.createReadStream(musicaPath, {start, end});
-    stream.pipe(res);
-});
+    const stream = fs.createReadStream(musicaPath, {start, end})
+    stream.pipe(res)
+})
 
 app.listen(3000, () => {
-    console.log('App rodando na porta 3000');
-});
+    console.log('App rodando na porta 3000')
+})
