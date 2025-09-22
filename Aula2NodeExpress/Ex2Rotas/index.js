8beba5820ff20cc0a5b4b187d3f1dd9d2e512f9e
harleymacedo <harley.ip@gmail.com>
2025-09-22 16:24:41 -0300

    Exemplo com leitura de json adicionado

--- Aula2NodeExpress/Ex2Rotas/index.js
+++ Aula2NodeExpress/Ex2Rotas/index.js
@@ -8,5 +8,24 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(professorRouter)
+app.get('/cidades', function (req, res) {
+    const fs = require('fs');
+    fs.readFile('./municipios.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Erro ao ler o arquivo:', err)
+        return
+    }
+    try {
+        const jsonData = JSON.parse(data) // Converte o conteúdo para objeto JS
+        console.log("Lista de cidades:")
+
+        jsonData.cidades.forEach(cidade => {
+        console.log(`- ${cidade.nome} (${cidade.estado})`)
+        })
+    } catch (parseError) {
+        console.error('Erro ao analisar o JSON:', parseError)
+    }
+    })
+})
 
 app.listen(3001)
