556fbc838d895ff3862800c732c13700832f8eb3
harleymacedo <harley.ip@gmail.com>
2025-09-24 16:29:07 -0300

    Exemplo de leitura de objetos json e filtragem

--- Aula2NodeExpress/Ex2Rotas/index.js
+++ Aula2NodeExpress/Ex2Rotas/index.js
@@ -1,24 +1,18 @@
 const express = require('express')
 const app = express()
 const professorRouter = require('./routes/professorRouter')
-const cors = require('cors')
-const fs = require('fs').promises
-
-app.use(cors({
-    origin: '*'
-}))
+const alunos = require('./dados/alunos')
 
 app.use(express.json())
 
 app.use(professorRouter)
 
-app.get('/capitais/todas', async function (req, res) {
-    const dados = await fs.readFile(__dirname + '/dados/municipios.json')
-    const dadosJson = await dados.json()
-    const dadosProcurados = dadosJson.map( function(itemAtual) {
-        return itemAtual.capital === 1
+app.get('/alunos/:cidade', function (req, res) {
+    const cidade = req.params.cidade
+    const alunosRequisitados = alunos.filter(  function (itemAtual) {
+        return itemAtual.cidade === cidade
     })
-    res.json({'cidadesCapitais': dadosProcurados})
+    res.json({"quantidade": alunosRequisitados.length, "alunos": alunosRequisitados})
 })
 
 app.listen(3000)
