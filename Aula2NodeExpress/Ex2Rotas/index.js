58f57042c30026de86c356db0e6a238132a774d5
harleymacedo <harley.ip@gmail.com>
2025-09-25 23:50:53 -0300

    Fazendo leitura de arquivo json

--- Aula2NodeExpress/Ex2Rotas/index.js
+++ Aula2NodeExpress/Ex2Rotas/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const professorRouter = require('./routes/professorRouter')
 const alunos = require('./dados/alunos')
+const fs = require('fs/promises')
 
 app.use(express.json())
 
@@ -13,6 +14,18 @@ app.get('/alunos/:cidade', function (req, res) {
         return itemAtual.cidade === cidade
     })
     res.json({"quantidade": alunosRequisitados.length, "alunos": alunosRequisitados})
+})
+
+app.get('/estados/todos', async function(req, res) {
+    try {
+        const conteudo = await fs.readFile(__dirname + '/dados/estados.json')
+        const dados = JSON.stringify(conteudo)
+        console.log(dados)
+        res.json({"mensagem": "Sem erro"})
+    } catch (error) {
+        console.log(error.message)
+        res.json({"mensagem": "Com erro"})
+    }
 })
 
 app.listen(3000)
