2dad63c49ff975ca72b245d178266d169a1a73b2
harleymacedo <harley.ip@gmail.com>
2026-03-10 09:06:42 -0300

    Middleware que usa escrita de arquivo consertado

--- Aula2NodeExpress/Ex3Middlewares/middlewares/logAtividade.js
+++ Aula2NodeExpress/Ex3Middlewares/middlewares/logAtividade.js
@@ -1,19 +1,22 @@
-//Função de middlwware que imprime dados da requisição quando alguma rota é acessada
+// Função de middleware que imprime dados da requisição quando alguma rota é acessada
+const fs = require('fs')
+const path = require('path')
+
 const registrarAtividade = (req, res, next) => {
-    let date = new Date()
-    let hora = date.toLocaleString('pt-br')
-    let texto = `Atividade: ${req.ip}, ${req.url}, ${hora}`
+    const date = new Date()
+    const hora = date.toLocaleString('pt-br')
+    const texto = `Atividade: ${req.ip}, ${req.method} ${req.url}, ${hora}`
+
     console.log(texto)
-    //Gerar arquivo
-    const fs = require('fs');
-    fs.writeFile('logs.txt', texto, (err) => {
+
+    // Anexa o log em um arquivo dentro da pasta do projeto (cria se não existir)
+    const logFilePath = path.join(__dirname, '..', 'logs.txt')
+    fs.appendFile(logFilePath, texto + '\n', (err) => {
         if (err) {
-            console.log(err);
-        } else {
-            console.log('Arquivo salvo com sucesso!');
+            console.error('Erro ao salvar log:', err)
         }
-    });
-    //Fim Gerar arquivo
+    })
+
     next()
 }
 
